a9392e8ce97c4a515f33f0ace494c3dac56ef3b5
aTran47 <97611396+aTran47@users.noreply.github.com>
2023-10-05 14:49:25 -0400

    Display activity launches textsizeadapter via registerForActivityResult

--- app/src/main/java/edu/temple/activities/DisplayActivity.kt
+++ app/src/main/java/edu/temple/activities/DisplayActivity.kt
@@ -1,10 +1,13 @@
 package edu.temple.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import androidx.activity.result.contract.ActivityResultContracts
 
+const val RESULT_KEY = "result"
 class DisplayActivity : AppCompatActivity() {
 
     // TODO Step 1: Launch TextSizeActivity when button clicked to allow selection of text size value
@@ -13,6 +16,9 @@ class DisplayActivity : AppCompatActivity() {
 
     private lateinit var lyricsDisplayTextView: TextView
     private lateinit var textSizeSelectorButton: Button
+    private val intentLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,6 +26,11 @@ class DisplayActivity : AppCompatActivity() {
 
         lyricsDisplayTextView = findViewById(R.id.lyricsDisplayTextView)
         textSizeSelectorButton = findViewById<Button?>(R.id.textSizeSelectorButton)
+
+        textSizeSelectorButton.setOnClickListener() {
+
+            intentLauncher.launch(Intent(this@DisplayActivity, TextSizeActivity::class.java))
+        }
 
     }
 }
